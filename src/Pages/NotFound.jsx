674e56486523dba81bffe2453a6b80ae84f859b6
Ms Sandy A <sandraallotey70@gmail.com>
2025-06-20 17:40:51 +0000

    translations'

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,27 +1,21 @@
-// import React from 'react'
-
-// export default function NotFound() {
-//   return (
-//     <div>
-      
-//     </div>
-//   )
-// }
 import React from "react";
 import notFound from "../assets/images/notfound.png";
 import { Link } from "react-router";
+import { useTranslation } from "react-i18next";
 
 const NotFound = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <div className="flex flex-col items-center justify-center">
-        <img src={notFound} alt="" className="w-full h-110  object-contain" />
-        <h1 className="font-bold text-6xl mt-2 mb-2 ">Oops!</h1>
+        <img src={notFound} alt="Not Found" className="w-full h-110 object-contain" />
+        <h1 className="font-bold text-6xl mt-2 mb-2">{t('Oops!')}</h1>
         <p className="text-gray-500 italic">
-          The page you are looking for does not exist.
+          {t('The page you are looking for does not exist.')}
         </p>
         <button className="text-blue-500 hover:underline mt-4 inline-block">
-          <Link to="/">Go back to Home</Link>
+          <Link to="/">{t('Go back to Home')}</Link>
         </button>
       </div>
     </div>
