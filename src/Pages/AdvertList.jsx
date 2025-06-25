b73e15598350f2e33a55d23b3ae42256dc82ce50
Manorkie25 <manorkie96@gmail.com>
2025-06-25 09:37:11 -0700

    update

---
+++ src/Pages/AdvertList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import craftsmanVintagelogo from "../assets/images/craftsmanVintagelogo.jpg";
+import { Link } from "react-router";
+import { apiFetcher } from '../api/client';
+import { BarLoader } from 'react-spinners';
+import AdvertCard from '../Components/AdvertCard';
+import useSWR from 'swr';
+
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function AdvertList() {
+  const { data, error, isLoading } = useSWR('/api/adverts', fetcher);
+
+  if (isLoading) return <p>Loading adverts...</p>;
+  if (error) return <p className="text-3xl text-center text-red-400 font-bold">Oh Oh! Failed to load adverts.</p>;
+
+
+  const today = new Date();
+  const validAdverts = data.filter(ad => new Date(ad.endDate) >= today);
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-4">
+     <Navbar />
+      {validAdverts.map((ad) => (
+        <AdvertCard
+          key={ad._id}
+          id={ad._id}
+          title={ad.title}
+          description={ad.description}
+          imageUrl={ad.image}
+          contact={ad.contact}
+          price={ad.price}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
